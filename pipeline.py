a19f8a6ee1a30aa32f60ae33d96f6b67da825a0e
nilsschillmann <nilsschillmann@googlemail.com>
2020-10-20 14:33:32 +0200

    implemented pyramid of hogs

--- pipeline.py
+++ pipeline.py
@@ -61,37 +61,40 @@ class pipeline():
     def run(self, img):
         
         if self.resize_factor:
-            img = self.resize_image(img)
+            img = pipeline.resize_image(img, self.resize_factor)
         
-        lab = self.convert2lab(img)
+        lab = pipeline.convert2lab(img)
         
-        working_sigmas = self.calculate_sigmas(self.sigmas)
+        working_sigmas = pipeline.calculate_sigmas(self.sigmas)
         
-        scalespaces = self.create_scalespaces(lab, working_sigmas)
+        scalespaces = pipeline.create_scalespaces(lab, working_sigmas)
         
-        differences = self.create_differences(scalespaces)
+        differences = pipeline.create_differences(scalespaces)
         
-        hogs = self.create_hogs(differences)
+        #hogs = self.create_hogs(differences)
+        feature_vector = self.create_feature_vector(differences, 3)
         
-        return np.concatenate(hogs)
+        return feature_vector
         
         
     
     @time_logger
-    def resize_image(self, img):
-        x, y = map(lambda a: round(a*self.resize_factor), img.shape[:2])
+    def resize_image(img, factor):
+        x, y = map(lambda a: round(a*factor), img.shape[:2])
         
         return resize(img, (x, y))
     
     @time_logger
-    def convert2lab(self, img):
+    def convert2lab(img):
+        # convert the image in a multichannel lab image
         converted = color.rgb2lab(img)
+        # split the channels
         channels = [converted[:,:,i] for i in
                     range(converted.shape[-1])]
         return channels
     
     @time_logger
-    def calculate_sigmas(self, sigmas):
+    def calculate_sigmas(sigmas):
         logging.info(f"sigmas = {sigmas}")
         s2 = lambda s1, s3 : sqrt(s3**2 - s1**2)
         working_sigmas = [sigmas[0]] # sigmas i have to add up
@@ -103,7 +106,7 @@ class pipeline():
         return working_sigmas
     
     @time_logger
-    def create_scalespaces(self, imgs, working_sigmas):
+    def create_scalespaces(imgs, working_sigmas):
         scalespaces = []
         for img in imgs:
             scalespace = [img]
@@ -117,7 +120,7 @@ class pipeline():
         return scalespaces
     
     @time_logger
-    def create_differences(self, scalespaces):
+    def create_differences(scalespaces):
         differences = []
         for scalespace in scalespaces:
             diffs = np.diff(np.array(scalespace), n=1, axis=0)
@@ -126,21 +129,32 @@ class pipeline():
         return np.concatenate(differences) 
 
 
-    @time_logger
-    def create_hogs(self, images):
+    #@time_logger
+    def create_hogs_pyramid(image, depth):
         
-        return list(map(lambda image: hog(
-                image, 
-                orientations=8, 
-                pixels_per_cell=(16, 16),
-                cells_per_block=(1, 1), 
-                visualize=False, 
-                multichannel=False,
-                feature_vector=True
-            ), images))
+        feature_vector = []
+        for i in [4**a for a in range(depth)]:
+            x, y = map(lambda a : a//i,image.shape[:2])
+            feature_vector.append(hog(image, orientations=8, pixels_per_cell=(x,y),cells_per_block=(1,1), multichannel=False))
+            
+        return np.concatenate(feature_vector)
+    
+    @time_logger
+    def create_feature_vector(self, differences, depth):
+        return np.concatenate(list(map(lambda a: pipeline.create_hogs_pyramid(a, depth), differences)))
+        
+        # return list(map(lambda image: hog(
+        #         image, 
+        #         orientations=8, 
+        #         pixels_per_cell=(16, 16),
+        #         cells_per_block=(1, 1), 
+        #         visualize=False, 
+        #         multichannel=False,
+        #         feature_vector=True
+        #     ), images))
     
 
-        
+    
 
     
 
