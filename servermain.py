fef2343bfc45b4e7de8244eaf130b20d00266214
Nils Schillmann <nilsschillmann@googlemail.com>
2020-12-14 17:52:36 +0100

    Refactor servermain

--- servermain.py
+++ servermain.py
@@ -3,7 +3,6 @@
 
 
 import logging
-import time
 import pickle
 import glob
 import sys
@@ -15,63 +14,55 @@ from PIL import Image
 from pipeline import Pipeline
 
 
-class Main:
-    '''Main class for the server'''
+def main():
+    '''Run the pipeline for all images in a folder'''
+    input_folder, \
+        output_folder, \
+        parameter = parse_config(sys.argv[1])
 
-    def __init__(self, config):
-        self.input_folder, \
-            self.output_folder, \
-            self.parameter = self.parse_config(config)
-        self.configure_logging(self.output_folder)
+    configure_logging(output_folder)
 
-    def main(self):
-        '''Run the pipeline for all images in a folder'''
+    picture_paths = glob.glob(input_folder + "/*")
+    file_sizes = []
+    for file in picture_paths:
+        img = Image.open(file)
+        file_sizes.append(img.size[0] * img.size[1])
 
-        picture_paths = glob.glob(self.input_folder + "/*")
+    print(file_sizes)
+    print()
+    print(max(file_sizes))
+    print(min(file_sizes))
 
-        file_sizes = []
-        for file in picture_paths:
-            im = Image.open(file)
-            file_sizes.append(im.size[0] * im.size[1])
 
-        print(file_sizes)
-        print()
-        print(max(file_sizes))
-        print(min(file_sizes))
+def configure_logging(folder):
+    '''configure the style and path for the logging file'''
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(filename=folder + '/test.log',
+                                  encoding='utf-8',
+                                  )
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s %(message)s'))
+    root_logger.addHandler(handler)
+    logging.info('logger set up')
 
-        #pipe = Pipeline()
 
-        #logging.info('parameter: {pipe.get_parameter()}')
+def parse_config(config_path):
+    '''load a specifig configuration from ini file'''
+    config = ConfigParser()
+    config.read(config_path)
+    input_folder = config.get('Folders', 'input')
+    output_folder = config.get('Folders', 'output')
+    parameter = {
+        'gauß_depth': config.getint('Parameter', 'gauss_depth'),
+        'hist_orientations': config.getint(
+            'Parameter', 'hist_orientations'),
+        'phog_depth': config.getint('Parameter', 'phog_depth'),
+        'resize_factor': config.getfloat('Parameter', 'resize_factor'),
+        }
 
-    def configure_logging(self, folder):
-        '''configure the style and path for the logging file'''
-        root_logger = logging.getLogger()
-        root_logger.setLevel(logging.INFO)
-        handler = logging.FileHandler(filename=folder + '/test.log',
-                                      encoding='utf-8',
-                                      )
-        handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s %(message)s'))
-        root_logger.addHandler(handler)
-        logging.info('logger set up')
-
-    def parse_config(self, config_path):
-        '''load a specifig configuration from ini file'''
-        config = ConfigParser()
-        config.read(config_path)
-        input_folder = config.get('Folders', 'input')
-        output_folder = config.get('Folders', 'output')
-        parameter = {
-            'gauß_depth': config.getint('Parameter', 'gauss_depth'),
-            'hist_orientations': config.getint(
-                'Parameter', 'hist_orientations'),
-            'phog_depth': config.getint('Parameter', 'phog_depth'),
-            'resize_factor': config.getfloat('Parameter', 'resize_factor'),
-            }
-
-        return input_folder, output_folder, parameter
+    return input_folder, output_folder, parameter
 
 
 if __name__ == '__main__':
-    main = Main(sys.argv[1])
-    main.main()
+    main()
